37cf4b7222282e1ce3fd9275a5bd5bbecbc5a3d9
Potwings <ygk1995@naver.com>
2023-08-25 22:21:12 +0900

    백준 10816번, 1764번, 1269번

---
+++ src/baekjoon/day40/Q10816.java
@@ -0,0 +1,66 @@
+package baekjoon.day40;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * 문제
+ * 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다.
+ * 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
+ * <p>
+ * 입력
+ * 첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다.
+ * 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+ * 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다.
+ * 넷째 줄에는 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다.
+ * 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
+ * <p>
+ * 출력
+ * 첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 몇 개 가지고 있는지를 공백으로 구분해 출력한다.
+ */
+
+/**
+ * 개선 사항
+ * 개수를 확인할 정수가 동일한 값이 들어있을 경우를 생각하지 않았다.
+ * Map으로 처리할 경우 Key에 동일한 값이 여러개 들어갈 수 없어 문제가 발생한다.
+ * 결과값 처리를 위한 별도의 배열을 추가해주었다.
+ *
+ * 처음에 카드의 개수를 Map에 저장한 후
+ * M 정수 값을 읽어올 때 비교하면 for loop(결과값 처리를 위한 배열 생성 과정)를 한 번 더 줄일 수 있다.
+ *
+ * 범위가 정해져있고 개수를 확인하는 문제이니 카운팅 정렬의 카운팅 방식으로 카운팅도 가능
+ */
+public class Q10816 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer, Integer> cardMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            Integer cardNum = Integer.parseInt(st.nextToken());
+            Integer count = cardMap.get(cardNum);
+            if (count == null) {
+                // Map에 없으면 추가
+                cardMap.put(cardNum, 1);
+            } else {
+                // Map에 있을 경우 +1
+                cardMap.put(cardNum, count + 1);
+            }
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            Integer count = cardMap.get(Integer.parseInt(st.nextToken()));
+            sb.append(count == null ? 0 : count).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/baekjoon/day40/Q1764.java
@@ -0,0 +1,56 @@
+package baekjoon.day40;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * 문제
+ * 김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+ * <p>
+ * 입력
+ * 첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다.
+ * 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다.
+ * 이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+ * <p>
+ * 듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
+ * <p>
+ * 출력
+ * 듣보잡의 수와 그 명단을 사전순으로 출력한다.
+ */
+public class Q1764 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 듣도 못한 사람 저장
+        Set<String> noListenSet = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            noListenSet.add(br.readLine());
+        }
+
+        // 듣도 못한 사람 Set에 저장되어 있는데 보도 못한 사람일 경우 resultSet에 추가
+        Set<String> resultSet = new HashSet<>();
+        for (int i = 0; i < M; i++) {
+            String noLookMan = br.readLine();
+            if (noListenSet.contains(noLookMan)) {
+                resultSet.add(noLookMan);
+            }
+        }
+
+        // 듣보잡 사전 순으로 정렬
+        String[] resultArr = resultSet.toArray(String[]::new);
+        Arrays.sort(resultArr);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(resultArr.length).append("\n");
+        for (String name : resultArr) {
+            sb.append(name).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
