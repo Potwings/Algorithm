e9a55743e7e98e3057b1de95f0ee6a625525881c
Potwings <ygk1995@naver.com>
2023-09-12 21:59:26 +0900

    백준 2444번

---
+++ src/baekjoon/day58/Q2444.java
@@ -0,0 +1,38 @@
+package baekjoon.day58;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q2444 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < N; i++) {
+            sb.append(printStar(N, i));
+        }
+
+        for (int i = N - 2; i >= 0; i--) {
+            sb.append(printStar(N, i));
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static String printStar(int N, int i) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = N - i; j > 1; j--) {
+            sb.append(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+            sb.append("*");
+        }
+
+        sb.append("\n");
+
+        return sb.toString();
+    }
+}
