c1cc23b3477034d314c62cbfa27c9237b841fe00
potwings <ygk1995@naver.com>
2023-09-19 08:21:30 +0900

    백준 10818번

---
+++ src/baekjoon/day65/Q10818.java
@@ -0,0 +1,37 @@
+package baekjoon.day65;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 문제
+ * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ * <p>
+ * 입력
+ * 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다.
+ * 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+ * <p>
+ * 출력
+ * 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+ */
+public class Q10818 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = -1000000;
+        int min = 1000000;
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(num, max);
+            min = Math.min(num, min);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(min).append(" ").append(max);
+        System.out.println(sb);
+    }
+}
