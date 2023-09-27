afa85a229bce1fdabc61bf6f3c9a59275b2d248f
Potwings <ygk1995@naver.com>
2023-09-27 20:46:10 +0900

    백준 2588번

---
+++ src/baekjoon/day73/Q2588.java
@@ -0,0 +1,27 @@
+package baekjoon.day73;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q2588 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        int tempNum = num2;
+        int digit1 = tempNum % 10;
+        tempNum /= 10;
+        int digit10 = tempNum % 10;
+        tempNum /= 10;
+        int digit100 = tempNum;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(num1 * digit1).append("\n")
+                .append(num1 * digit10).append("\n")
+                .append(num1 * digit100).append("\n")
+                .append(num1 * num2);
+
+        System.out.println(sb);
+    }
+}
