3cebf8c0791cc2e9135f6f7b1b584c6904ab6d08
Potwings <ygk1995@naver.com>
2023-09-28 15:09:04 +0900

    백준 25304번

---
+++ src/baekjoon/day74/Q25304.java
@@ -0,0 +1,24 @@
+package baekjoon.day74;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q25304 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        int total = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            total += a * b;
+        }
+
+        System.out.println(total == X ? "Yes" : "No");
+    }
+}
