6e0d71a8f18f0d836e2cc7b7a43e2e5c01475b78
Potwings <ygk1995@naver.com>
2023-10-18 23:08:56 +0900

    백준 10872번

---
+++ src/baekjoon/day94/Q10872.java
@@ -0,0 +1,17 @@
+package baekjoon.day94;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q10872 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 1;
+        for(int i = 2; i <= N; i++){
+            result = result * i;
+        }
+
+        System.out.println(result);
+    }
+}
