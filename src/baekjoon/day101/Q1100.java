2bfa2cb2a8b3a9e4dfb678600e07d25a6d75f20a
Potwings <ygk1995@naver.com>
2023-10-25 21:29:20 +0900

    백준 1100번

---
+++ src/baekjoon/day101/Q1100.java
@@ -0,0 +1,26 @@
+package baekjoon.day101;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q1100 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+        for (int i = 0; i < 8; i++) {
+            String input = br.readLine();
+            int start = 0;
+            if (i % 2 > 0) {
+                // 홀수번째 줄일경우 두번째 칸부터 하얀 칸이다.
+                start = 1;
+            }
+            for (int j = start; j < 8; j += 2) {
+                if (input.charAt(j) == ('F')) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
