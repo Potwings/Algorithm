bc1ac9ed7235d08f38b2421c43ead493545318f4
Potwings <ygk1995@naver.com>
2023-10-28 09:47:30 +0900

    백준 1032번

---
+++ src/baekjoon/day104/Q1032.java
@@ -0,0 +1,32 @@
+package baekjoon.day104;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q1032 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] fileNames = new String[N];
+        for (int i = 0; i < N; i++) {
+            fileNames[i] = br.readLine();
+        }
+        StringBuilder sb = new StringBuilder();
+        String firstFile = fileNames[0];
+        check:
+        for (int i = 0; i < firstFile.length(); i++) {
+            char c = firstFile.charAt(i);
+            for (int j = 1; j < N; j++) {
+                if (c != fileNames[j].charAt(i)) {
+                    // 이전의 값과 다를 경우 ?를 추가한 후 다음 문자열을 확인한다.
+                    sb.append("?");
+                    continue check;
+                }
+            }
+            sb.append(c);
+        }
+
+        System.out.println(sb);
+    }
+}
