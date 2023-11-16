ee17f3717463bcba8f8394eef4668eceb3f520da
Potwings <ygk1995@naver.com>
2023-11-16 22:00:50 +0900

    백준 2338번

---
+++ src/baekjoon/day123/Q2338.java
@@ -0,0 +1,17 @@
+package baekjoon.day123;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Q2338 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(A.add(B)).append("\n").append(A.subtract(B)).append("\n").append(A.multiply(B));
+        System.out.println(sb);
+    }
+}
