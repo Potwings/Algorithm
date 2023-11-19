fc564183f9ad2b0e65e33972971b3e2d0bc8b8ef
Potwings <ygk1995@naver.com>
2023-11-19 22:36:37 +0900

    백준 1920번(개선), 2178번(개선), 27294번

--- src/baekjoon/day124/Q2178.java
+++ src/baekjoon/day124/Q2178.java
@@ -2,12 +2,19 @@ package baekjoon.day124;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+/**
+ * 개선 사항
+ * 인접리스트를 생성하는 것이 아닌 이동 시에 이동할 수 있는 좌표를 확인한다.
+ * 이동할 수 있는 경우는 4가지로 한정되어있으므로 배열로 생성하여 처리한다.
+ */
 public class Q2178 {
-    static List<Position>[][] adjPositionListArr;
-
+    static boolean[][] maze;
     static boolean[][] visit;
+    static int[][] count;
 
     static int N;
 
@@ -20,105 +27,61 @@ public class Q2178 {
         M = Integer.parseInt(st.nextToken());
 
         // 미로 배열 저장
-        boolean[][] maze = new boolean[N + 2][M + 2]; // 마지막 인접리스트 검사까지 고려하여 + 2로 생성
+        maze = new boolean[N + 2][M + 2]; // 첫번쨰 줄과 마지막 줄까지 고려하여 +2로 진행
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < M; j++) {
                 if (input.charAt(j) == '1') {
-                    maze[i + 1][j + 1] = true;
+                    maze[i+1][j+1] = true;
                 }
             }
         }
+
+        count = new int[N + 1][M + 1];
         // 방문 배열 생성
         visit = new boolean[N + 1][M + 1];
-        // 인접 리스트 생성
-        adjPositionListArr = new ArrayList[N + 1][M + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                adjPositionListArr[i][j] = new ArrayList<Position>();
-                if (maze[i + 1][j]) {
-                    adjPositionListArr[i][j].add(new Position(i + 1, j));
-                }
-                if (maze[i][j + 1]) {
-                    adjPositionListArr[i][j].add(new Position(i, j + 1));
-                }
-                if (maze[i - 1][j]) {
-                    adjPositionListArr[i][j].add(new Position(i - 1, j));
-                }
-                if (maze[i][j - 1]) {
-                    adjPositionListArr[i][j].add(new Position(i, j - 1));
-                }
-            }
-        }
 
         // (1,1)부터 시작
-        int result = BFS(new Position(1, 1));
-        System.out.println(result);
+        BFS(1, 1);
     }
 
-    public static int BFS(Position position) {
+    public static int BFS(int row, int column) {
 
-        Queue<Position> queue = new LinkedList<>();
-        position.count = 1;
+        Queue<int[]> queue = new LinkedList<>();
         // 큐에 추가
-        queue.offer(position);
+        queue.offer(new int[]{row, column});
+
         // 방문 처리
-        position.changeVisit();
+        visit[row][column] = true;
+
+        // 방문칸 수 저장
+        count[row][column] = 1;
+
+        // 한 칸에서 가능한 좌표가 변화할 수 있는 경우의 수
+        int[] dRow = {0, 1, 0, -1};
+        int[] dColumn = {1, 0, -1, 0};
 
         // 큐 탐색
         while (!queue.isEmpty()) {
-            Position p = queue.poll();
-            if (p.isEndPoint()) {
-                return p.count;
+            int[] now = queue.poll();
+            int nowRow = now[0];
+            int nowColumn = now[1];
+            if (nowRow == N && nowColumn == M) {
+                System.out.println(count[nowRow][nowColumn]);
             }
-            for (Position adjPosition : p.getAdjPositionList()) {
-                if (!adjPosition.isVisit()) {
-                    // 방문한 적 없을 경우 count 증가 후 큐에 추가
-                    adjPosition.count = p.count + 1;
-                    adjPosition.changeVisit();
-                    queue.offer(adjPosition);
+
+            for (int i = 0; i < 4; i++) {
+                int afterRow = nowRow + dRow[i];
+                int afterColumn = nowColumn + dColumn[i];
+                if (maze[afterRow][afterColumn] && !visit[afterRow][afterColumn]) {
+                    // 길이 있고 방문한 적이 없을 경우
+                    visit[afterRow][afterColumn] = true;
+                    count[afterRow][afterColumn] = count[nowRow][nowColumn] + 1;
+                    queue.offer(new int[]{afterRow, afterColumn});
                 }
             }
         }
 
         return 0; // 방문할 수 없는 경우는 없다.
-    }
-
-    public static class Position {
-        int row;
-
-        int column;
-
-        int count; // 현재까지 이동한 칸 수
-
-        public void changeVisit() {
-            visit[row][column] = true;
-        }
-
-        public boolean isEndPoint() {
-            return row == N && column == M;
-        }
-
-        public boolean isVisit() {
-            return visit[row][column];
-        }
-
-        public List<Position> getAdjPositionList() {
-            return adjPositionListArr[row][column];
-        }
-
-        public Position(int row, int column) {
-            this.row = row;
-            this.column = column;
-        }
-
-        @Override
-        public String toString() {
-            return "Position{" +
-                    "row=" + row +
-                    ", column=" + column +
-                    ", count=" + count +
-                    '}';
-        }
     }
 }
---
+++ src/baekjoon/day126/Q27294.java
@@ -0,0 +1,21 @@
+package baekjoon.day126;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q27294 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int result = 280;
+        if (T >= 12 && T <= 16 && S != 1) {
+            result = 320;
+        }
+
+        System.out.println(result);
+    }
+}
