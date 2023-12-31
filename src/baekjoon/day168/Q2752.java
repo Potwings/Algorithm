e3cba0b859a650ec5f0823f38daf9076f306b1c5
Potwings <ygk1995@naver.com>
2023-12-31 23:22:57 +0900

    백준 2752번

---
+++ src/baekjoon/day168/Q2752.java
@@ -0,0 +1,24 @@
+package baekjoon.day168;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q2752 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] nums = new int[3];
+    for (int i = 0; i < 3; i++) {
+      nums[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(nums);
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 3; i++) {
+      sb.append(nums[i]).append(" ");
+    }
+    System.out.println(sb);
+  }
+}
