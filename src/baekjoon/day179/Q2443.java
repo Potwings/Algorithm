bf34ddc590dc2ed72f8ea09b7f080bafb7c27c25
Potwings <ygk1995@naver.com>
2024-01-11 22:27:19 +0900

    백준 2443번

---
+++ src/baekjoon/day179/Q2443.java
@@ -0,0 +1,17 @@
+package baekjoon.day179;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q2443 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    for (int i = N; i >= 1; i--) {
+      sb.append(" ".repeat(N - i)).append("*".repeat(2 * (i - 1) + 1)).append("\n");
+    }
+    System.out.println(sb);
+  }
+}
