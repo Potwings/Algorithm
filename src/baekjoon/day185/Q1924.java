32d2e6b75610e32b06ae7c41a439083427b77f2f
potwings <ygk1995@naver.com>
2024-01-17 12:38:49 +0900

    백준 1924번

---
+++ src/baekjoon/day185/Q1924.java
@@ -0,0 +1,27 @@
+package baekjoon.day185;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1924 {
+
+  static final int[] monthArr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 월별 마지막일
+  static final String[] dayArr = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int month = Integer.parseInt(st.nextToken());
+    int day = Integer.parseInt(st.nextToken());
+
+    int dayCount = 0;
+    for (int i = 0; i < month - 1; i++) {
+      dayCount += monthArr[i];// 해당 월까지의 지난 일자 계산
+    }
+
+    dayCount += day - 1; // 해당 일까지의 지난 일자 계산
+
+    System.out.println(dayArr[dayCount % 7]); // 7로 나눈 나머지에 따라 요일 확인
+  }
+}
