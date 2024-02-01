9798ab43e87615397e7488c9d297cda853cb6c50
potwings <ygk1995@naver.com>
2024-02-01 17:21:31 +0900

    백준 1264번

---
+++ src/baekjoon/day200/Q1264.java
@@ -0,0 +1,28 @@
+package baekjoon.day200;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q1264 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Character[] vowelArr = {'a', 'e', 'i', 'o', 'u'};
+    Set<Character> vowels = new HashSet<>(Arrays.asList(vowelArr));
+    String input;
+    while (!(input = br.readLine().toLowerCase()).equals("#")) {
+      int count = 0;
+      for (char c : input.toCharArray()) {
+        if (vowels.contains(c)) {
+          count++;
+        }
+      }
+      System.out.println(count);
+    }
+
+  }
+
+}
