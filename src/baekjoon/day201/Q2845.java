7ddef9cfd89fd70f2989b379fbd547a5c9ad9a4c
potwings <ygk1995@naver.com>
2024-02-02 19:00:30 +0900

    백준 2845번

---
+++ src/baekjoon/day201/Q2845.java
@@ -0,0 +1,20 @@
+package baekjoon.day201;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2845 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int total = Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()); // 상근 총 인원 수
+    st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 5; i++) {
+      sb.append(Integer.parseInt(st.nextToken()) - total).append(" "); // 각 신문 인원 수와의 차이
+    }
+    System.out.println(sb);
+  }
+}
