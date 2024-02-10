7b1702a0eab33a31975b9ad767158a277b3e5b11
Potwings <ygk1995@naver.com>
2024-02-10 23:10:18 +0900

    백준 9316번

---
+++ src/baekjoon/day209/Q9316.java
@@ -0,0 +1,17 @@
+package baekjoon.day209;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q9316 {
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    for(int i = 1; i <= N; i++) {
+      sb.append("Hello World, Judge ").append(i).append("!").append("\n");
+    }
+
+    System.out.println(sb);
+  }
+}
