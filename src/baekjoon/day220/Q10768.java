94724dfc4f1ce0c0144dac276ec9f234c58467d4
Potwings <ygk1995@naver.com>
2024-02-21 22:31:34 +0900

    백준 10768번

---
+++ src/baekjoon/day220/Q10768.java
@@ -0,0 +1,21 @@
+package baekjoon.day220;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q10768 {
+
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int month = Integer.parseInt(br.readLine());
+    int day = Integer.parseInt(br.readLine());
+    int date = month * 100 + day;
+    if(date > 218) {
+      System.out.println("After");
+    } else if (date < 218) {
+      System.out.println("Before");
+    } else {
+      System.out.println("Special");
+    }
+  }
+}
