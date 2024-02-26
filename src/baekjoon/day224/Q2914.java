a1ea9140e5bde320f64a9fa8f09bb73a928b6220
Potwings <ygk1995@naver.com>
2024-02-26 21:16:30 +0900

    백준 13909번

--- src/baekjoon/day224/Q2914.java
+++ src/baekjoon/day224/Q2914.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Q13909 {
+public class Q2914 {
 
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
